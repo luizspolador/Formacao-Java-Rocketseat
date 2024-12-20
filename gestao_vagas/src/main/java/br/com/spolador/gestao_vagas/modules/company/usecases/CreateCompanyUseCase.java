db74a3a419dd8a5d55fa056c726e80c1dbbd1dfb
luizspolador <luizh.spolador@gmail.com>
2024-12-20 13:52:35 -0300

    adicionando JWT

--- gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/company/usecases/CreateCompanyUseCase.java
+++ gestao_vagas/src/main/java/br/com/spolador/gestao_vagas/modules/company/usecases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import br.com.spolador.gestao_vagas.exceptions.UserFoundException;
 import br.com.spolador.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.spolador.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,16 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity){
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
         return this.companyRepository.save(companyEntity);
     }
 }
